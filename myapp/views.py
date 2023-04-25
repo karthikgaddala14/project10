45e7b0b6dd699b3e9fbda6e21611e8adae55989e
karthikgaddala14 <131743112+karthikgaddala14@users.noreply.github.com>
2023-04-25 11:20:17 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -5,3 +5,7 @@ def welcome(request):
     res=HttpResponse("""<html><body bgcolor=green><
                     h1><center>myapp space karthik</center></h1></body></html>""")
     return res
+def welcome(request):
+    res=HttpResponse("""<html><body bgcolor=green><
+                    h1><center>myapp space karthik</center></h1></body></html>""")
+    return res
