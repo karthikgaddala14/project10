fee573ef59cdfb721bb664b0223590c4de1b9ef8
praveenshravan7761 <kumarshravan67481@gmail.com>
2023-04-25 11:19:10 +0530

    Initial commit

---
+++ myapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def welcome(request):
+    res=HttpResponse("""<html><body bgcolor=green><
+                    h1><center>myapp space karthik</center></h1></body></html>""")
+    return res
